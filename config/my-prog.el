9946b464350f6e6f24392e8806ec771383874e0d
Andrew Gray <adsgray@gmail.com>
2016-12-28 09:43:26 -0800

    go-mode-hook to set compile-command

--- config/my-prog.el
+++ config/my-prog.el
@@ -5,4 +5,8 @@
 (add-hook 'prog-mode-hook #'auto-highlight-symbol-mode)
 (add-hook 'prog-mode-hook #'wrap-region-mode)
 
+(add-hook 'go-mode-hook (progn
+			  (setq compile-command "go build")
+			  ))
+
 (provide 'my-prog)
