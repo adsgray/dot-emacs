d628bd91548a024cb390e6e266154f8abb3538e3
Andrew Gray <adsgray@gmail.com>
2016-12-30 15:57:22 -0800

    fix stuff by putting it into 'after-init-hook hook

--- config/my-hooks.el
+++ config/my-hooks.el
@@ -1,6 +1,15 @@
 
 ;; company (complete anything, autocompletion in all buffers)
-(add-hook 'after-init-hook 'global-company-mode)
+(add-hook 'after-init-hook 'my-company-hooks)
+
+(defun my-company-hooks ()
+  ""
+  (global-company-mode)
+  (add-to-list 'company-backends 'company-go)
+  (add-to-list 'company-backends 'company-php)
+  (add-to-list 'company-backends 'company-python)
+  )
+
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
 
 (provide 'my-hooks)
