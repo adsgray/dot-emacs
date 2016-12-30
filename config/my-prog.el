fab71ebbce34fbfdb35ae613e31a6611aea50f44
Andrew Gray <adsgray@gmail.com>
2016-12-30 15:58:30 -0800

    prog related kekybindings and electric-pair-mode

--- config/my-prog.el
+++ config/my-prog.el
@@ -5,9 +5,16 @@
 (add-hook 'prog-mode-hook #'auto-highlight-symbol-mode)
 (add-hook 'prog-mode-hook #'wrap-region-mode)
 
-(add-hook 'go-mode-hook (progn
-			  (setq compile-command "go build")
-			  (add-hook 'before-save-hook 'gofmt-before-save)
-			  ))
+;;(add-hook 'go-mode-hook (lambda () ( (setq compile-command "go build"))))
+
+(add-hook 'before-save-hook 'gofmt-before-save)
+
+(require 'go-complete)
+(add-hook 'completion-at-point-functions 'go-complete-at-point)
+
+(electric-pair-mode)
+
+(global-set-key (kbd "C-, c") 'compile)
+(global-set-key (kbd "C-, r") 'recompile)
 
 (provide 'my-prog)
